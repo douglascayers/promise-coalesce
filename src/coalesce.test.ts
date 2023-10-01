7a653025bc07efe971104d3efb0e6dbbfdd794ff
Doug Ayers <douglascayers@gmail.com>
2023-09-30 20:48:20 -0500

    test: add test to demonstrate the problem

--- src/coalesce.test.ts
+++ src/coalesce.test.ts
@@ -1,13 +1,60 @@
 import { coalesceAsync } from './coalesce';
 
-describe('#coalesceAsync', () => {
+const coalescingFunc = (key: string, fn: () => PromiseLike<string>) => {
+  return coalesceAsync(key, fn);
+};
+
+const nonCoalescingFunc = (_key: string, fn: () => PromiseLike<string>) => {
+  return fn();
+};
+
+// This demonstrates the problem.
+describe('non-coalescing function', () => {
+  const funcToTest = nonCoalescingFunc;
+
+  test('given ten requests for same key then function executed ten times and resolved to all requests', async () => {
+    const fn = jest.fn().mockImplementation(async () => {
+      return 'some value';
+    });
+
+    // Simulate several concurrent requests for the same value.
+    const results = await Promise.allSettled([
+      funcToTest('some key', fn), // 1
+      funcToTest('some key', fn), // 2
+      funcToTest('some key', fn), // 3
+      funcToTest('some key', fn), // 4
+      funcToTest('some key', fn), // 5
+      funcToTest('some key', fn), // 6
+      funcToTest('some key', fn), // 7
+      funcToTest('some key', fn), // 8
+      funcToTest('some key', fn), // 9
+      funcToTest('some key', fn), // 10
+    ]);
+
+    // Assert that the function was called multiple times (bad).
+    expect(fn).toBeCalledTimes(10);
+
+    // Assert that all requests resolved to the same value.
+    results.forEach((result) => {
+      expect(result).toMatchObject({
+        status: 'fulfilled',
+        value: 'some value',
+      });
+    });
+  });
+});
+
+// This demonstrates the solution.
+describe('coalescing function', () => {
+  const funcToTest = coalescingFunc;
+
   test('given one request then function executed once and resolves', async () => {
     const fn = jest.fn().mockImplementation(async () => {
       return 'some value';
     });
 
     // Simulate a single request for a value.
-    const results = await Promise.allSettled([coalesceAsync('some key', fn)]);
+    const results = await Promise.allSettled([funcToTest('some key', fn)]);
 
     // Assert that the function was called exactly once.
     expect(fn).toBeCalledTimes(1);
@@ -27,7 +74,7 @@ describe('#coalesceAsync', () => {
     });
 
     // Simulate a single request for a value.
-    const results = await Promise.allSettled([coalesceAsync('some key', fn)]);
+    const results = await Promise.allSettled([funcToTest('some key', fn)]);
 
     // Assert that the function was called exactly once.
     expect(fn).toBeCalledTimes(1);
@@ -48,16 +95,16 @@ describe('#coalesceAsync', () => {
 
     // Simulate several concurrent requests for the same value.
     const results = await Promise.allSettled([
-      coalesceAsync('some key', fn), // 1
-      coalesceAsync('some key', fn), // 2
-      coalesceAsync('some key', fn), // 3
-      coalesceAsync('some key', fn), // 4
-      coalesceAsync('some key', fn), // 5
-      coalesceAsync('some key', fn), // 6
-      coalesceAsync('some key', fn), // 7
-      coalesceAsync('some key', fn), // 8
-      coalesceAsync('some key', fn), // 9
-      coalesceAsync('some key', fn), // 10
+      funcToTest('some key', fn), // 1
+      funcToTest('some key', fn), // 2
+      funcToTest('some key', fn), // 3
+      funcToTest('some key', fn), // 4
+      funcToTest('some key', fn), // 5
+      funcToTest('some key', fn), // 6
+      funcToTest('some key', fn), // 7
+      funcToTest('some key', fn), // 8
+      funcToTest('some key', fn), // 9
+      funcToTest('some key', fn), // 10
     ]);
 
     // Assert that the function was called exactly once.
@@ -80,16 +127,16 @@ describe('#coalesceAsync', () => {
 
     // Simulate several concurrent requests for the same value.
     const results = await Promise.allSettled([
-      coalesceAsync('some key', fn), // 1
-      coalesceAsync('some key', fn), // 2
-      coalesceAsync('some key', fn), // 3
-      coalesceAsync('some key', fn), // 4
-      coalesceAsync('some key', fn), // 5
-      coalesceAsync('some key', fn), // 6
-      coalesceAsync('some key', fn), // 7
-      coalesceAsync('some key', fn), // 8
-      coalesceAsync('some key', fn), // 9
-      coalesceAsync('some key', fn), // 10
+      funcToTest('some key', fn), // 1
+      funcToTest('some key', fn), // 2
+      funcToTest('some key', fn), // 3
+      funcToTest('some key', fn), // 4
+      funcToTest('some key', fn), // 5
+      funcToTest('some key', fn), // 6
+      funcToTest('some key', fn), // 7
+      funcToTest('some key', fn), // 8
+      funcToTest('some key', fn), // 9
+      funcToTest('some key', fn), // 10
     ]);
 
     // Assert that the function was called exactly once.
@@ -112,11 +159,11 @@ describe('#coalesceAsync', () => {
 
     // Simulate a first batch of requests.
     const results1 = await Promise.allSettled([
-      coalesceAsync('some key', fn), // 1
-      coalesceAsync('some key', fn), // 2
-      coalesceAsync('some key', fn), // 3
-      coalesceAsync('some key', fn), // 4
-      coalesceAsync('some key', fn), // 5
+      funcToTest('some key', fn), // 1
+      funcToTest('some key', fn), // 2
+      funcToTest('some key', fn), // 3
+      funcToTest('some key', fn), // 4
+      funcToTest('some key', fn), // 5
     ]);
 
     // Assert that the function was called exactly once.
@@ -133,11 +180,11 @@ describe('#coalesceAsync', () => {
 
     // Simulate a second batch of requests.
     const results2 = await Promise.allSettled([
-      coalesceAsync('some key', fn), // 1
-      coalesceAsync('some key', fn), // 2
-      coalesceAsync('some key', fn), // 3
-      coalesceAsync('some key', fn), // 4
-      coalesceAsync('some key', fn), // 5
+      funcToTest('some key', fn), // 1
+      funcToTest('some key', fn), // 2
+      funcToTest('some key', fn), // 3
+      funcToTest('some key', fn), // 4
+      funcToTest('some key', fn), // 5
     ]);
 
     // Assert that the function was called exactly once.
